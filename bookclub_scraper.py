da72b3f16c68e4646a05075bd1faa049bdf94def
mfdalgetty <107519009+mfdalgetty@users.noreply.github.com>
2024-02-02 08:31:08 -0800

    removed html storage and added screenshots

--- bookclub_scraper.py
+++ bookclub_scraper.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 import time
-import io
 import os
 
 # Initializing some web driver options
@@ -20,10 +19,10 @@ options.add_experimental_option('detach', True)
 data = pd.read_excel('book_club_books.xlsx').drop(index=0)
 books = data['Title'].tolist()
 
-# Create a folder to store html files
+# Create a folder to store reviews
 
-folder = "html_files"
-os.makedirs(folder, exist_ok=True)
+output_folder = "reviews"
+os.makedirs(output_folder, exist_ok=True)
 
 # Begin scraping goodreads for each book
 
@@ -31,9 +30,6 @@ driver = webdriver.Chrome(options=options)
 
 try: 
     for book in books:
-
-        # Create path to export html files
-        file_path = os.path.join(folder, f"{book}.html")
 
         # Open driver and search for book
         driver.get('https://www.goodreads.com')
@@ -52,45 +48,17 @@ try:
         # Select title and get source information
         title = driver.find_element(By.CLASS_NAME, "bookTitle")
         title.click()
-        time.sleep(3)
+        WebDriverWait(driver, 5)
         html = driver.page_source
-        time.sleep(1)
+        WebDriverWait(driver, 5)
 
-        # Prettify html and save it as a file
-        soup = BeautifulSoup(html, 'html.parser')
-        html = soup.prettify()
-        with io.open(file_path, "w", encoding="utf-8") as f:
-            f.write(html)
+        # Pass source information to Parser and prettify it
+        prettified_html = BeautifulSoup(html, 'html.parser').prettify()
+        soup = BeautifulSoup(prettified_html, 'html.parser')
+
+        # Find reviews and set up path to store them
         review_sections = soup.find_all('section', class_='ReviewText')
-
-finally:
-    driver.quit()
-
-# Scrape reviews from stored html pages
-    
-for book in books:
-
-    try:
-
-        # Specify the file path and create a folder to store reviews
-        input_file = f'html_files/{book}.html'
-        output_folder = "reviews"
-        os.makedirs(output_folder, exist_ok=True)
-
-        # Create path to export reviews
         file_path = os.path.join(output_folder, f"{book} reviews.xlsx")
-
-        # Read the content of the HTML file
-        with io.open(input_file, 'r', encoding='utf-8') as file:
-            html_content = file.read()
-
-        # Parse the HTML content with BeautifulSoup
-        soup = BeautifulSoup(html_content, 'html.parser')
-
-        # Find all sections with class "ReviewText"
-        review_sections = soup.find_all('section', class_='ReviewText')
-
-        # Create an empty list to store reviews
         reviews_list = []
 
         # Iterate through each review section
@@ -109,6 +77,9 @@ for book in books:
 
         # Display the Excel file path
         print(f'{book} Reviews DataFrame has been exported to: {output_folder}')
-    
-    except Exception as e:
+
+except Exception as e:
         print(f"Error processing {book}: {str(e)}")
+
+finally:
+    driver.quit()
